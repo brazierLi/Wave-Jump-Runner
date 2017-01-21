79cdb050e7bacdc5eb6d479e69e1c31c012aa304
brazierLi <liam.brazier1@gmail.com>
2017-01-21 12:39:15 +0000

    new
    new new

--- Assets/game/Camer_Controller.cs
+++ Assets/game/Camer_Controller.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Camer_Controller : MonoBehaviour {
 
-    public float yOffset = 5, zOffset = -10;
+    public float yOffset = 5, zOffset = -10,xOffSEt = 0.6f;
     GameObject Player;
     void Start() {
         Player = GameObject.Find("Player");
@@ -13,6 +13,6 @@ public class Camer_Controller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = new Vector3(Player.transform.position.x, yOffset, zOffset); ;
+       GetComponent<Rigidbody2D>().velocity = Player.transform.position - transform.position + new Vector3(Player.GetComponent<Rigidbody2D>().velocity.x*xOffSEt, yOffset, zOffset); ;
     }
 }
